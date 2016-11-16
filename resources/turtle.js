d6c4feec1defb6e57e8ea1de4b377952960a0493
Braden Pellowski <bradenpellowski@bradenpellowski.wireless.dartmouth.edu>
2016-11-16 16:04:00 -0500

    cleaner

--- resources/turtle.js
+++ resources/turtle.js
@@ -4,14 +4,6 @@ var Turtle = function(canvasId, ctx){
 	this.lParameter = 100;
 	this.selectedModel = 0;
 
-	//this.lParameter = 500;
-	//this.tParameter = 3;
-    // Setup all the data related to the actual curve.
-    // this.nodes = new Array();
-    // this.showControlPolygon = true;
-    // this.showAdaptiveSubdivision = true;
-    // this.showSubEnds = true;
-    this.tDepth = 2;
     
     // Set up all the data related to drawing the curve
     this.cId = canvasId;
@@ -24,12 +16,6 @@ var Turtle = function(canvasId, ctx){
     }
     this.computeCanvasSize();
     
-    // Setup event listeners
-    this.cvState = CVSTATE.Idle;
-    this.activeNode = null;
-    
-    // closure
-    var that = this;
     if(this.selectedModel==0){
 	    this.xorigin = (this.dCanvas.width/2) - (.5*this.lParameter);
 	    this.yorigin = (this.dCanvas.height/2) + (this.lParameter/12);
@@ -53,24 +39,6 @@ var Turtle = function(canvasId, ctx){
 
     this.degreeToRadian = Math.PI/180;
     
-    // Event listeners
-    //this.dCanvas.addEventListener('resize', this.computeCanvasSize());
-    
-    // this.dCanvas.addEventListener('mousedown', function(event) {
-    //     that.mousePress(event);
-    // });
-    
-    // this.dCanvas.addEventListener('mousemove', function(event) {
-    //     that.mouseMove(event);
-    // });
-    
-    // this.dCanvas.addEventListener('mouseup', function(event) {
-    //     that.mouseRelease(event);
-    // });
-    
-    // this.dCanvas.addEventListener('mouseleave', function(event) {
-    //     that.mouseRelease(event);
-    // });
 }
 
 
@@ -115,51 +83,37 @@ Turtle.prototype.move = function(d){
 	this.x = xn;
 	this.y = yn;
 
-	//console.log("check");
 
 	return(this);
 }
 
 
 Turtle.prototype.left = function(an){
+	
 	this.a -= an*this.degreeToRadian;
-
 	return(this);
 }
 
 Turtle.prototype.right = function(an){
+	
 	this.a += an*this.degreeToRadian;
-
 	return(this);
 }
 
 Turtle.prototype.back = function(){
+	
 	this.pd = false;
-
 	return(this);
 }
 
 Turtle.prototype.setA = function(an){
 
 	this.a = this.degreeToRadian*an;
-
 	return(this);
-
 }
 
 Turtle.prototype.draw = function(){
-	//this.move(15);
-	//this.setA(60);
-	//this.move(15).left(60).move(15).right(120);
-	//console.log("check 2");
-	// this.move(this.unit);
-	// this.lParametereft(60);
-	// this.move(this.unit);
-	// this.right(120);
-	// this.move(this.unit);
-	// this.lParametereft(60);
-	// this.move(this.unit);
-	// return(this);
+	
 	this.ctx.clearRect(0, 0, this.dCanvas.width, this.dCanvas.height);
 	if(this.selectedModel==0){
 		this.drawKochCurve(this.tParameter,this.lParameter);
